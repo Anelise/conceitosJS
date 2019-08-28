08f45a6caef29ca1e829ba1f9e16fc755548380a
Anelise <ane.schunk@gmail.com>
2019-08-28 13:40:47 -0300

    criado percorreAttrObjeto.js
    As vezes vocÊ não sabe o nome correto do atributo mas precisa de seu valor, use for in para encontrar essa informação

---
+++ percorreAttrObjeto.js
@@ -0,0 +1,8 @@
+/**
+Quando vocês quer percorrer os atributos de um objeto você pode usar o 'for in'
+**/
+
+let umobj = {nome:'Maria',idade:'20',sexo:'feminio'}
+for( chave in umobj){
+	console.log(chave, umobj[chave]);
+}
