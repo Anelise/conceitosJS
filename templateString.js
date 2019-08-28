dd15e7aa885d16309fb06093c3773656127af29f
Anelise <ane.schunk@gmail.com>
2019-08-28 13:46:42 -0300

    criado templateString.js
    criar string interativamente

---
+++ templateString.js
@@ -0,0 +1,6 @@
+/**
+O javascript tem uma forma de construir strings mais inteligente - chamam isso de tempate string
+**/
+
+var casa = 'casa rosa';
+console.log(`lalalala ${1+1} - ${casa}`) //feito com o sinal de crase
